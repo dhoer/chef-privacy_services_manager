45aad4b391d280cca0a1a83f6eaf5217c4e7a1e9
Dennis Hoer <dennis.hoer@pearson.com>
2015-07-30 12:46:42 -0700

    Fix invalid file stub

--- spec/unit/default_spec.rb
+++ spec/unit/default_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe 'privacy_services_manager::default' do
   before(:each) do
-    allow(File).to receive(:exists?).and_call_original
-    allow(File).to receive(:exists?).and_return(false)
+    allow(File).to receive(:exist?).and_call_original
+    allow(File).to receive(:exist?).and_return(false)
   end
 
   let(:chef_run) do
@@ -11,6 +11,6 @@ describe 'privacy_services_manager::default' do
   end
 
   it 'installs privacy_services_manager' do
-    expect(chef_run).to_not install_dmg_package('Privacy Services Management [1.6.8]')
+    expect(chef_run).to install_dmg_package('Privacy Services Management [1.6.8]')
   end
 end
