33eb2634aafb7c134d3d90cdfe1031220dc16675
Dennis Hoer <dennis.hoer@pearson.com>
2015-07-30 22:04:18 -0700

    Bump privacy_services_manager from 1.6.8 to 1.6.9

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 1.0.1
+
+- Bump privacy_services_manager from 1.6.8 to 1.6.9
+
 ## 1.0.0
 
 - Initial release
--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['privacy_services_manager']['version'] = '1.6.8'
+default['privacy_services_manager']['version'] = '1.6.9'
 default['privacy_services_manager']['checksum'] = '6224519a7974dc61b04599ea94018643189425a3402266f20a5c139d9469e823'
 
 default['privacy_services_manager']['management_tools']['version'] = '1.8.1'
