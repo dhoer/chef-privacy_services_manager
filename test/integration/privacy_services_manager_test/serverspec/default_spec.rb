664a5eb58c47be801ad7e5676f96f7c3bbe24738
Dennis Hoer <dennis.hoer@pearson.com>
2016-05-01 21:43:42 -0700

    Fix serverspec paths

--- test/integration/privacy_services_manager_test/serverspec/default_spec.rb
+++ test/integration/privacy_services_manager_test/serverspec/default_spec.rb
@@ -11,17 +11,17 @@ describe 'privacy_services_manager_test' do
       it { should be_owned_by 'root' }
     end
 
-    describe file('/Users/vagrant/Library/Application Support') do
+    describe file("/Users/#{ENV['USER']}/Library/Application Support") do
       it { should be_directory }
       it { should be_owned_by ENV['USER'] }
     end
 
-    describe file('/Users/vagrant/Library/Application Support/com.apple.TCC') do
+    describe file("/Users/#{ENV['USER']}/Library/Application Support/com.apple.TCC") do
       it { should be_directory }
       it { should be_owned_by ENV['USER'] }
     end
 
-    describe file('/Users/vagrant/Library/Application Support/com.apple.TCC/TCC.db') do
+    describe file("/Users/#{ENV['USER']}/Library/Application Support/com.apple.TCC/TCC.db") do
       it { should be_file }
       it { should be_owned_by ENV['USER'] }
     end
